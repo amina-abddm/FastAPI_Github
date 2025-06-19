0a3ac550cdde4d1bd94971be27df86827582008b
amina_abddm <amina.abd.75@gmail.com>
2025-06-19 17:42:04 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
 from api.routes import router  # import des router défini dans routes.py
-from api.security import verify_token  # import de la fonction de vérification du token
-from api.models import User  # import du modèle User
 
-app = APIRouter()
+app = FastAPI()
 
 # 🔗 Enregistre les routes
 app.include_router(router)
